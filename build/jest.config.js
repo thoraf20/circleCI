42e3abc3f2b8ad8e8d58d37edbb911970b6c931c
Rauf <decagon@Decagons-MacBook-Air.local>
2020-09-29 17:13:13 +0100

    test

---
+++ build/jest.config.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = {
+    roots: ["<rootDir>/build"],
+    testMatch: [
+        "**/__tests__/**/*.+(ts|tsx|ts)",
+        "**/?(*.)+(spec|test).+(ts|tsx|ts)",
+    ],
+    transform: {
+        "^.+\\.(ts|tsx|ts)$": "ts-jest",
+    },
+    testEnvironment: "node",
+    globalSetup: './setupTests.js',
+    globalTeardown: './teardownTests.js',
+};
